9b031f8f9814abd33cfa4e7aec01532bad6cfc68
SSoDm <163316535+SSoDm@users.noreply.github.com>
2024-07-13 08:13:27 +0700

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -1,1 +1,23 @@
+// Библиотека, созданная как для команд для МИР
 
+import { Color } from 'pixel_combats/basic';
+import { Teams } from 'pixel_combats/room';
+
+export var RED_TEAM_NAME = "Red";
+export var BLUE_TEAM_NAME = "Blue";
+export var RED_TEAM_DISPLAY_NAME = "Teams/Red";
+export var BLUE_TEAM_DISPLAY_NAME = "Teams/Blue";
+export var BLUE_TEAM_SPAWN_POINTS_GROUP = 1;
+export var RED_TEAM_SPAWN_POINTS_GROUP = 2;
+
+export function create_team_red() {
+    Teams.Add(RED_TEAM_NAME, RED_TEAM_DISPLAY_NAME, new Color(1, 0, 0, 0));
+    Teams.Get(RED_TEAM_NAME).Spawns.SpawnPointsGroups.Add(RED_TEAM_SPAWN_POINTS_GROUP);
+    return Teams.Get(RED_TEAM_NAME);
+}
+
+export function create_team_blue() {
+    Teams.Add(BLUE_TEAM_NAME, BLUE_TEAM_DISPLAY_NAME, new Color(0, 0, 1, 0));
+    Teams.Get(BLUE_TEAM_NAME).Spawns.SpawnPointsGroups.Add(BLUE_TEAM_SPAWN_POINTS_GROUP);
+    return Teams.Get(BLUE_TEAM_NAME);
+}
