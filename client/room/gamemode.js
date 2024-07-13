327bad9510ea2094f3eecf3bbaf270bda225ea8e
SSoDm <163316535+SSoDm@users.noreply.github.com>
2024-07-13 07:53:05 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,4 +1,6 @@
 //var System = importNamespace('System');
+import { Color } from 'pixel_combats/basic';
+import { Damage, BreackGraph, Build, Ui, Teams, Inventory, Spawns, GameMode } from 'pixel_combats/room';
 
 // Настройки, опции 
 Damage.GetContext().DamageOut.Value = GameMode.Parameters.GetBool("Damage");
@@ -9,11 +11,11 @@ Build.GetContext().FillQuad.Value = GameMode.Parameters.GetBool("FillQuad");
 Build.GetContext().RemoveQuad.Value = GameMode.Parameters.GetBool("RemoveQuad");
 Build.GetContext().FlyEnable.Value = GameMode.Parameters.GetBool("Fly");
 
-// 
+// Делаем возможность, ломать все блоки  
 BreackGraph.BreackAll = true;
-// ���������� ���������� ������
+// Показываем, количество квадов 
 Ui.GetContext().QuadsCount.Value = true;
-// ��� ������������ �����
+// Опции 
 Build.GetContext().Pipette.Value = true;
 Build.GetContext().BalkLenChange.Value = true;
 Build.GetContext().SetSkyEnable.Value = true;
@@ -26,48 +28,46 @@ Build.GetContext().RenameMapEnable.Value = true;
 Build.GetContext().ChangeMapAuthorsEnable.Value = true;
 Build.GetContext().LoadMapEnable.Value = true;
 Build.GetContext().ChangeSpawnsEnable.Value = true;
-
-// ��������� ����
-Properties.GetContext().GameModeName.Value = "GameModes/Peace";
-// ������� �������
+// Создаём, команды
 red = GameMode.Parameters.GetBool("RedTeam");
 blue = GameMode.Parameters.GetBool("BlueTeam");
 if (red || !red && !blue) {
-	Teams.Add("Red", "Teams/Red", { r: 1 });
+	Teams.Add("Red", "Teams/Red", new Color(1, 0, 0, 0));
 	Teams.Get("Red").Spawns.SpawnPointsGroups.Add(2);
 }
 if (blue || !red && !blue) {
-	Teams.Add("Blue", "Teams/Blue", { b: 1 });
+	Teams.Add("Blue", "Teams/Blue", new Color(0, 0, 1, 0));
 	Teams.Get("Blue").Spawns.SpawnPointsGroups.Add(1);
 	if(GameMode.Parameters.GetBool("BlueHasNothing")){
-		var inventory = Inventory.GetContext();
+		var Inventory = Inventory.GetContext();
 		Teams.Get("Blue").Inventory.Main.Value = false;
 		Teams.Get("Blue").Inventory.Secondary.Value = false;
 		Teams.Get("Blue").Inventory.Melee.Value = false;
 		Teams.Get("Blue").Inventory.Explosive.Value = false;
 		Teams.Get("Blue").Inventory.Build.Value = false;
+
 	}
 }
 
-// ��������� ���� � ������� �� �������
+// Разрешаем, вход в команды
 Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);});
-// ����� �� ����� � �������
+// Спавн по вход, в команду
 Teams.OnPlayerChangeTeam.Add(function(player){ player.Spawns.Spawn()});
 
-// ������ ���������
-Ui.getContext().Hint.Value = "Hint/BuildBase";
+// Задаём, подсказку 
+Ui.GetContext().Hint.Value = "!Мирный режим!";
 
-// ������������ ���������
-var inventory = Inventory.GetContext();
-inventory.Main.Value = false;
-inventory.Secondary.Value = false;
-inventory.Melee.Value = true;
-inventory.Explosive.Value = false;
-inventory.Build.Value = true;
-inventory.BuildInfinity.Value = true;
+// Конфигурация, инвентаря
+var Inventory = Inventory.GetContext();
+Inventory.Main.Value = false;
+Inventory.Secondary.Value = false;
+Inventory.Melee.Value = true;
+Inventory.Explosive.Value = false;
+Inventory.Build.Value = true;
+Inventory.BuildInfinity.Value = true;
 
-// ��������� ��� ������ �����
+// Делаем, все чистые блоки 
 Build.GetContext().BlocksSet.Value = BuildBlocksSet.AllClear;
 
-// ������������ �����
+// Моментальный, спавн 
 Spawns.GetContext().RespawnTime.Value = 0;
