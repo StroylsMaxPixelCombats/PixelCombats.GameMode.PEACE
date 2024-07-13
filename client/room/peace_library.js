28439a5a469d6aa25ca3383572d972b4efbb03a5
SSoDm <163316535+SSoDm@users.noreply.github.com>
2024-07-13 14:24:46 +0700

    Update peace_library.js

--- client/room/peace_library.js
+++ client/room/peace_library.js
@@ -2,70 +2,90 @@
 import * as room from 'pixel_combats/room';
 import * as teams from './default_teams.js';
 
-// Настройки, опции 
-Damage.GetContext().DamageOut.Value = GameMode.Parameters.GetBool("Damage");
-BreackGraph.OnlyPlayerBlocksDmg = GameMode.Parameters.GetBool("PartialDesruction");
-BreackGraph.WeakBlocks = GameMode.Parameters.GetBool("LoosenBlocks");
-Build.GetContext().FloodFill.Value = GameMode.Parameters.GetBool("FloodFill");
-Build.GetContext().FillQuad.Value = GameMode.Parameters.GetBool("FillQuad");
-Build.GetContext().RemoveQuad.Value = GameMode.Parameters.GetBool("RemoveQuad");
-Build.GetContext().FlyEnable.Value = GameMode.Parameters.GetBool("Fly");
-
-// Делаем возможность, ломать все блоки  
-BreackGraph.BreackAll = true;
-// Показываем, количество квадов 
-Ui.GetContext().QuadsCount.Value = true;
-// Опции 
-Build.GetContext().Pipette.Value = true;
-Build.GetContext().BalkLenChange.Value = true;
-Build.GetContext().SetSkyEnable.Value = true;
-Build.GetContext().GenMapEnable.Value = true;
-Build.GetContext().ChangeCameraPointsEnable.Value = true;
-Build.GetContext().QuadChangeEnable.Value = true;
-Build.GetContext().BuildModeEnable.Value = true;
-Build.GetContext().CollapseChangeEnable.Value = true;
-Build.GetContext().RenameMapEnable.Value = true;
-Build.GetContext().ChangeMapAuthorsEnable.Value = true;
-Build.GetContext().LoadMapEnable.Value = true;
-Build.GetContext().ChangeSpawnsEnable.Value = true;
-
-// Игровое, состояние
-Properties.GetContext().GameModeName.Value = "GameModes/Peace";
-// Создаём, команды
-var red = GameMode.Parameters.GetBool("RedTeam");
-var blue = GameMode.Parameters.GetBool("BlueTeam");
-if (red || !red && !blue) teams.create_team_red();
-if (blue || !red && !blue) teams.create_team_blue();
-	if(GameMode.Parameters.GetBool("BlueHasNothing")){
-		var Inventory = Inventory.GetContext();
-		Teams.Get("Blue").Inventory.Main.Value = false;
-		Teams.Get("Blue").Inventory.Secondary.Value = false;
-		Teams.Get("Blue").Inventory.Melee.Value = false;
-		Teams.Get("Blue").Inventory.Explosive.Value = false;
-		Teams.Get("Blue").Inventory.Build.Value = false;
-
-	}
+// Разрешаем все что можно, для строительства
+function set_inventory() {
+    const context = room.Inventory.GetContext();
+    context.Main.Value = false;
+    context.Secondary.Value = false;
+    context.Melee.Value = true;
+    context.Explosive.Value = false;
+    context.Build.Value = true;
+    context.BuildInfinity.Value = true;
 }
 
-// Разрешаем, вход в команды
-Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);});
-// Спавн по вход, в команду
-Teams.OnPlayerChangeTeam.Add(function(player){ player.Spawns.Spawn()});
+function set_build_settings() {
+    const context = room.Build.GetContext();
+    // Прочие, опции
+    context.Pipette.Value = true;
+    context.BalkLenChange.Value = true;
+    context.SetSkyEnable.Value = true;
+    context.GenMapEnable.Value = true;
+    context.ChangeCameraPointsEnable.Value = true;
+    context.QuadChangeEnable.Value = true;
+    context.BuildModeEnable.Value = true;
+    context.CollapseChangeEnable.Value = true;
+    context.RenameMapEnable.Value = true;
+    context.ChangeMapAuthorsEnable.Value = true;
+    context.LoadMapEnable.Value = true;
+    context.ChangeSpawnsEnable.Value = true;
+    context.BlocksSet.Value = room.BuildBlocksSet.AllClear; // делаем возможность строительства всеми блоками
+}
 
-// Задаём, подсказку 
-Ui.GetContext().Hint.Value = "!Мирный, режим!";
+// Задаём в контекст инвентаря, пустой инвентарь
+function set_empty_inventory(inventory) {
+    inventory.Main.Value = false;
+    inventory.Secondary.Value = false;
+    inventory.Melee.Value = false;
+    inventory.Explosive.Value = false;
+    inventory.Build.Value = false;
+}
 
-// Конфигурация, инвентаря
-var Inventory = Inventory.GetContext();
-Inventory.Main.Value = false;
-Inventory.Secondary.Value = false;
-Inventory.Melee.Value = true;
-Inventory.Explosive.Value = false;
-Inventory.Build.Value = true;
-Inventory.BuildInfinity.Value = true;
+// Задаём опции режима мир, выбранные при создании, комнаты
+export function apply_room_options() {
+    const gameModeParameters = room.GameMode.Parameters;
 
-// Делаем, все чистые блоки 
-Build.GetContext().BlocksSet.Value = BuildBlocksSet.AllClear;
+    // Опции, строительства
+    const buildContext = room.Build.GetContext();
+    buildContext.FloodFill.Value = gameModeParameters.GetBool("FloodFill");
+    buildContext.FillQuad.Value = gameModeParameters.GetBool("FillQuad");
+    buildContext.RemoveQuad.Value = gameModeParameters.GetBool("RemoveQuad");
+    buildContext.FlyEnable.Value = gameModeParameters.GetBool("Fly");
 
-// Моментальный, спавн 
-Spawns.GetContext().RespawnTime.Value = 0;
+    // Прочие, опции
+    room.Damage.GetContext().DamageOut.Value = gameModeParameters.GetBool("Damage");
+    room.BreackGraph.OnlyPlayerBlocksDmg = gameModeParameters.GetBool("PartialDesruction");
+    room.BreackGraph.WeakBlocks = gameModeParameters.GetBool("LoosenBlocks");
+}
+
+// Задаём, настройки режима мир
+export function configure() {
+    room.Properties.GetContext().GameModeName.Value = "GameModes/Peace";// задаем название режима
+    room.Ui.GetContext().Hint.Value = "Hint/BuildBase";// выводим подсказку
+    room.Ui.GetContext().QuadsCount.Value = true;// выводим количество квадов на карте
+    room.BreackGraph.BreackAll = true; // делаем так, чтобы можно было сломать любой блок
+    room.Spawns.GetContext().RespawnTime.Value = 0; // убираем таймер респавна
+    set_build_settings();
+    set_inventory();
+    apply_room_options();
+}
+
+export function create_teams() {
+    // Создаём, команды
+    const roomParameters = room.GameMode.Parameters;
+    const hasRedTeam = roomParameters.GetBool("RedTeam");
+    const hasBlueTeam = roomParameters.GetBool("BlueTeam");
+    if (hasRedTeam || !hasRedTeam && !hasBlueTeam) {
+        teams.create_team_red();
+    }
+    if (hasBlueTeam || !hasRedTeam && !hasBlueTeam) {
+        const blueTeam = teams.create_team_blue();
+        if (roomParameters.GetBool("BlueHasNothing")) {
+            set_empty_inventory(blueTeam.Inventory);
+        }
+    }
+
+    // По запросу на вход, в команду - кидаем игрока в команду
+    room.Teams.OnRequestJoinTeam.Add(function (player, team) { team.Add(player); });
+    // если игрок сменил команду, или выбрал ее, то происходит спавн игрока
+    room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn() });
+}
