4b8f224cb6e68e0ef7a7c908065e90575686a90f
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2024-11-27 18:17:46 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -5,10 +5,10 @@ import { Inventory, BreackGraph, Properties, Damage, Teams, Ui, Build, Spawns, G
 Damage.GetContext().DamageOut.Value = GameMode.Parameters.GetBool("Damage");
 BreackGraph.OnlyPlayerBlocksDmg = GameMode.Parameters.GetBool("PartialDesruction");
 BreackGraph.WeakBlocks = GameMode.Parameters.GetBool("LoosenBlocks");
-Build.GetContext().FloodFill.Value = GameMode.Parameters.GetBool("FloodFill");
-Build.GetContext().FillQuad.Value = GameMode.Parameters.GetBool("FillQuad");
-Build.GetContext().RemoveQuad.Value = GameMode.Parameters.GetBool("RemoveQuad");
-Build.GetContext().FlyEnable.Value = GameMode.Parameters.GetBool("Fly");
+Build.getContext().FloodFill.Value = GameMode.Parameters.GetBool("FloodFill");
+Build.getContext().FillQuad.Value = GameMode.Parameters.GetBool("FillQuad");
+Build.getContext().RemoveQuad.Value = GameMode.Parameters.GetBool("RemoveQuad");
+Build.getContext().FlyEnable.Value = GameMode.Parameters.GetBool("Fly");
 
 // Делаем возможно - ломать все блоки:
 BreackGraph.BreackAll = true;
@@ -34,28 +34,28 @@ Build.getContext().ChangeSpawnsEnable.Value = true;
 Build.getContext().BuildRangeEnable.Value = true;
 
 // Параметры, игры:
-Properties.GetContext().GameModeName.Value = "GameModes/Peace";
+Properties.getContext().GameModeName.Value = "GameModes/Peace";
 // Стандартные - команды:
 Teams.Add("Blue", "Teams/Blue", new Color(0, 0, 1, 0));
 Teams.Add("Red", "Teams/Red", new Color(1, 0, 0, 0));
 var BlueTeam = Teams.Get("Blue");
 var RedTeam = Teams.Get("Red");
-BlueTeam.Get("Blue").Spawns.SpawnPointsGroups.Add(1);
-RedTeam.Get("Red").Spawns.SpawnPointsGroups.Add(2);
+BlueTeam.get("Blue").Spawns.SpawnPointsGroups.Add(1);
+RedTeam.get("Red").Spawns.SpawnPointsGroups.Add(2);
 // Настройки:
-if (GameMode.Parameters.GetBool("BlueHasNothing")) {
- var inventory = Inventory.GetContext();
- Teams.Get("BlueTeam").inventory.Main.Value = false;
- Teams.Get("BlueTeam").inventory.Secondary.Value = false;
- Teams.Get("BlueTeam").inventory.Melee.Value = false;
- Teams.Get("BlueTeam").inventory.Explosive.Value = false; 
- Teams.Get("BlueTeam").inventory.Build.Value = false;
+if (GameMode.Parameters.getBool("BlueHasNothing")) {
+ var inventory = Inventory.getContext();
+ Teams.get("BlueTeam").inventory.Main.Value = false;
+ Teams.get("BlueTeam").inventory.Secondary.Value = false;
+ Teams.get("BlueTeam").inventory.Melee.Value = false;
+ Teams.get("BlueTeam").inventory.Explosive.Value = false; 
+ Teams.get("BlueTeam").inventory.Build.Value = false;
 }
-if (GameMode.Parameters.GetBool("BlueTeam")) {
- var BlueTeam = Teams.Get("Blue");
+if (GameMode.Parameters.getBool("BlueTeam")) {
+ var BlueTeam = Teams.get("Blue");
 }
-if (GameMode.Parameters.GetBool("BlueTeam")) {
- var RedTeam = Teams.Get("Red");
+if (GameMode.Parameters.getBool("BlueTeam")) {
+ var RedTeam = Teams.get("Red");
 }
 // Разрешаем, вход в команды - по запросу:
 Teams.OnRequestJoinTeam.Add(function(Player,Team){Team.Add(Player);});
@@ -63,10 +63,10 @@ Teams.OnRequestJoinTeam.Add(function(Player,Team){Team.Add(Player);});
 Teams.OnPlayerChangeTeam.Add(function(Player){ Player.Spawns.Spawn()});
 
 // Задаём, подсказку - игроку:
-Ui.GetContext().Hint.Value = "Hint/BuildBase";
+Ui.getContext().Hint.Value = "Hint/BuildBase";
 
 // Конфигурация - инвентаря:
-var inventory = Inventory.GetContext();
+var inventory = Inventory.getContext();
 inventory.Main.Value = false;
 inventory.Secondary.Value = false;
 inventory.Melee.Value = true;
@@ -75,7 +75,7 @@ inventory.Build.Value = true;
 inventory.BuildInfinity.Value = true;
 
 // Моментальный - спавн:
-Spawns.GetContext().RespawnTime.Value = 0;
+Spawns.getContext().RespawnTime.Value = 0;
 
 // Разрешаем, игрокам - чистые блоки:
 Build.BlocksSet.Value = BuildBlocksSet.AllClear;
