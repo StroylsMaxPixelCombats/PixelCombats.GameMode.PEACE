2cbe90cbbfdb032f847bc95af46e5638b05426eb
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2024-12-04 10:13:54 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -19,8 +19,6 @@ Teams.Add("Blue", "Teams/Blue", new Color(0, 0, 1, 0));
 Teams.Add("Red", "Teams/Red", new Color(1, 0, 0, 0));
 var BlueTeam = Teams.Get("Blue");
 var RedTeam = Teams.Get("Red");
-BlueTeam.Get("Blue").Spawns.SpawnPointsGroups.Add(2);
-RedTeam.Get("Red").Spawns.SpawnPointsGroups.Add(1);
 
 // Делаем возможно - ломать все блоки:
 BreackGraph.BreackAll = true;
