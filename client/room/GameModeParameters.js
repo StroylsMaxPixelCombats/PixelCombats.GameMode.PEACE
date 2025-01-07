7f0ad2e21f4ccef2b722f99fb427bff94bdc3628
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2025-01-07 21:57:30 +0700

    Update and rename Удалено to client/room/GameModeParameters.js

---
+++ client/room/GameModeParameters.js
@@ -0,0 +1,8 @@
+import { GameMode } from 'pixel_combats/room';
+
+export function TeamsCreator() { 
+ var RedTeam = GameMode.Parameters.GetBoll("Red");
+ var BlueTeam = GameMode.Parameters.GetBool("BlueTeam");
+}
+
+
